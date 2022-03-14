5fb4afc5bdef31eeb4f6c1f48769c9d5a78dcb15
sahilsharma0223 <sahilsharma4373@gmail.com>
2022-03-14 22:05:23 +0530

    initial commit

---
+++ resource/resource/views.py
@@ -0,0 +1,153 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+import os
+from django.contrib.auth import authenticate, login, logout
+from django.core.files.storage import FileSystemStorage
+from user_view.models import Member
+from material.models import Material
+from wsgiref.util import FileWrapper
+import mimetypes
+
+def explore(request,id):
+    #mat1 = Material.objects.raw('SELECT * FROM material')
+    mat = Material.objects.raw('SELECT * FROM material WHERE id=%s',[id])
+    print(mat[0].link==None, mat[0].document, mat[0].video)
+    if(mat[0].link!=None):
+        return redirect(mat[0].link)
+    else:
+        fs = FileSystemStorage()
+        if(mat[0].document!=""):
+            filename = mat[0].document
+        else:
+            filename = mat[0].video
+        filename=str(filename)
+        name=filename[12:]
+        filepath = os.path.join(fs.location, filename)
+        try:
+            wrapper = FileWrapper(open(filepath, 'rb'))
+            content_type = mimetypes.guess_type(filepath)[0]
+            response = HttpResponse(wrapper, content_type=content_type)
+            response['Content-Length'] = os.path.getsize(filepath)
+            response['Content-Disposition'] = "attachment; filename=%s" % name
+            return response
+        except:
+            return HttpResponse("File not found")   
+    #return render(request, 'home.html', {'mat': mat1})
+
+def refineddatat(request,mat):
+    user = request.user
+    member = Member.objects.get(usn=user.username)
+    liked = member.likes.all()
+    disliked = member.dislikes.all()
+    report = member.report.all()
+
+    lst = []
+    for i in mat:
+        temp = {}
+        temp['id'] = i.id
+        temp['title'] = i.title
+        temp['description'] = i.description
+        temp['type'] = i.type
+        temp['thumbnail'] = i.thumbnail
+        temp['semester'] = i.semester
+        temp['link'] = i.link
+        temp['document'] = i.document
+        temp['video'] = i.video
+        temp['like_count'] = i.like_count
+        temp['dislike_count'] = i.dislike_count
+        temp['isliked'] = False
+        temp['isdisliked'] = False
+        temp['isreported'] = False
+
+        lst.append(temp)
+
+        for i in liked:
+            for j in lst:
+                if(i.id == j['id']):
+                    j['isliked'] = True
+        for i in disliked:
+            for j in lst:
+                if(i.id == j['id']):
+                    j['isdisliked'] = True
+        for i in report:
+            for j in lst:
+                if(i.id == j['id']):
+                    j['isreported'] = True
+        for i in lst:
+            print(i['isliked'], i['isdisliked'], i['isreported'])
+    return lst
+
+
+def home(request):
+    mat = Material.objects.all()
+    if request.user.is_authenticated==False:
+        print("not logged in")
+        render(request, 'home.html', {'mat': mat})
+    elif request.user.is_superuser:
+        render(request, 'home.html', {'mat': mat})
+    else:
+        mat=refineddatat(request,mat)
+    return render(request, 'home.html', {'mat': mat})
+    
+
+def userLogout(request):
+    logout(request)
+    return redirect('/')
+
+
+def userLogin(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user_ob = User.objects.filter(username=username).first()
+        print(user_ob)
+
+        if user_ob is None:
+            msg = {'msg': 'User does not exist'}
+            print(msg)
+            return render(request, 'login_register.html', { "msg": msg, "page": page })
+
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is None:
+            msg = {'msg': 'Invalid Credentials'}
+            print(msg)
+            return render(request,'login_register.html', {'msg': msg, "page": page})
+
+        login(request, user)
+        return redirect('/')
+    return render(request, 'login_register.html', {'page': page})
+
+
+def userRegister(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+
+        if User.objects.filter(email=email).first() is not None:
+            msg = {'msg': 'Email already exists'}
+            print(msg)
+            return render(request, 'login_register.html', {'msg2': msg, "page": "register"})
+        elif User.objects.filter(username=username).first() is not None:
+            msg = {'msg': 'Username already exists'}
+            print(msg)
+            return render(request, 'login_register.html', {'msg2': msg, "page": "register"})
+        else:
+            user_ob = User.objects.create(
+                username=username, email=email, first_name=first_name, last_name=last_name)
+            user_ob.set_password(password)
+            user_ob.save()
+            login(request, user_ob)
+            member=Member.objects.create(usn=username,first_name=first_name,last_name=last_name,email=email)
+            member.save()
+            return redirect('/')
+    return render(request, 'login_register.html', {'page': "register"})
---
+++ resource/user_view/views.py
@@ -0,0 +1,180 @@
+from django.shortcuts import render,redirect
+from numpy import issubsctype
+
+from material.models import Material
+from resource.views import refineddatat
+from user_view.models import notification
+from user_view.models import issues
+from user_view.models import Member
+from django.core.paginator import Paginator
+
+# Create your views here.
+
+def issue_delete(request,id):
+    print(id)
+    issue=issues.objects.get(issue_id=id)
+    issue.delete()
+    return redirect("/issuestatus")
+
+def profile(request):
+    if request.user.is_authenticated==False:
+        return redirect('/login/')
+    member=Member.objects.filter(usn=request.user.username).first()
+    return render(request,'profile.html',{'member':member})
+
+def youruploads(request):
+    if request.user.is_authenticated==False:
+        return redirect('/login/')
+    usn=request.user.username
+    youruploads=Member.objects.get(usn=usn)
+    youruploads=youruploads.material.all()
+    youruploads=refineddatat(request,youruploads)
+    print(youruploads)
+    return render(request,'home.html',{'mat':youruploads})
+
+def issue(request):
+    if request.user.is_authenticated==False:
+        return redirect('/login/')
+    if request.method=='POST':
+        category=request.POST['category']
+        desc=request.POST['desc']
+        usn=request.user.username
+        member=Member.objects.get(usn=usn)
+        issue=issues(category=category,desc=desc,issue_status="Pending",usn=member)
+        issue.save()
+        return redirect('/issuestatus/')
+    return render(request,'issues.html')
+
+def issuestatus(request):
+    if request.user.is_authenticated==False:
+        return redirect('/login/')
+    issue = issues.objects.filter(usn=request.user.username)
+    paginator = Paginator(issue, 1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'issuestatus.html', {'issues': page_obj})
+        
+def reportedcontent(request):
+    if request.user.is_authenticated==False:
+        return redirect('/login/')
+    usn=request.user.username
+    reportedcontent=Member.objects.get(usn=usn)
+    reportedcontent=reportedcontent.report.all() 
+    reportedcontent=refineddatat(request,reportedcontent)
+    return render(request,'home.html',{'mat':reportedcontent})
+
+def notifications(request):
+    if request.user.is_authenticated==False:
+        return redirect('/login/')
+    usn=request.user.username
+    notifications=notification.objects.filter(usn=usn)
+
+    return render(request,'notifications.html',{'notifications':notifications})
+
+def removenotification(request,id):
+    if request.user.is_authenticated==False:
+        return redirect('/login/')
+    notification.objects.get(notification_id=id).delete()
+    return redirect('/notifications/')
+
+
+def searchdata(searchtext):
+    searchwords = searchtext.split()
+    if len(searchwords) > 0:
+        matdict = {}
+        for word in searchwords:
+            material = Material.objects.filter(title__icontains=word)
+            for i in material:
+                if i in matdict.keys():
+                    matdict[i] += 1
+                else:
+                    matdict[i] = 1
+            print(matdict)
+        matdict = sorted(matdict.items(), key=lambda x: x[1], reverse=True)
+        matlist = []
+        for i in matdict:
+            matlist.append(i[0])
+    return matlist
+
+
+def searching(request):
+    material = Material.objects.all()
+    
+    if request.method == 'POST':
+        searchtext = request.POST['searchtext']
+        if request.user.is_authenticated == False:
+            matlist=searchdata(searchtext)
+            return render(request, "home.html", {"mat": matlist})
+        elif request.user.is_superuser:
+            matlist = searchdata(searchtext)
+            return render(request, "home.html", {"mat": matlist})
+        else:
+            matlist = searchdata(searchtext)
+            matlist=refineddatat(request,matlist)
+            
+            if len(matlist)>0:
+                return render(request, 'home.html', {'mat':matlist})
+            else:
+                return render(request, 'home.html',{'error': 'No results found'})
+    
+    return render(request, 'home.html', {'mat': material})
+
+def like(request,id):
+    if request.user.is_authenticated==False:
+        return redirect('/login/')
+    material=Material.objects.get(id=id)
+    member=Member.objects.get(usn=request.user.username)
+    if material in member.likes.all():
+        member.likes.remove(material)
+        material.like_count -= 1
+    elif material in member.dislikes.all():
+        member.dislikes.remove(material)
+        member.likes.add(material)
+        material.like_count += 1
+        material.dislike_count -= 1
+    else:
+        member.likes.add(material)
+        material.like_count += 1
+    material.save()
+    return redirect('/')
+
+def dislike(request,id):
+    if request.user.is_authenticated==False:
+        return redirect('/login/')
+    material = Material.objects.get(id=id)
+    member = Member.objects.get(usn=request.user.username)
+    if material in member.dislikes.all():
+        member.dislikes.remove(material)
+        material.dislike_count -= 1
+    elif material in member.likes.all():
+        member.likes.remove(material)
+        member.dislikes.add(material)
+        material.like_count -= 1
+        material.dislike_count += 1
+    else:
+        member.dislikes.add(material)
+        material.dislike_count += 1
+    material.save()
+    return redirect('/')
+
+def report(request,id):
+    if request.user.is_authenticated==False:
+        return redirect('/login/')
+    material = Material.objects.get(id=id)
+    member = Member.objects.get(usn=request.user.username)
+    if material in member.report.all():
+        member.report.remove(material)
+    else:
+        member.report.add(material)
+    material.save()
+    return redirect('/')
+
+
+def filter(request):
+    material = Material.objects.all()
+    if request.method == 'POST':
+        sem = request.POST['semester']
+        matlist =  Material.objects.raw('SELECT * FROM material WHERE semester = %s',sem)
+        print((matlist))
+        return render(request, "home.html", {"mat": matlist})
+    return render(request, "home.html", {"mat": material})
